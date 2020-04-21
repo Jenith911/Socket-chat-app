b36d05ecfd0edb1e266142ee4b1d96abcaf170a1
Jenith <jenithds@gmail.com>
2020-04-21 12:19:54 +0530

    Emit NewMessage on createMessage

--- Server/server.js
+++ Server/server.js
@@ -15,14 +15,14 @@ app.use(express.static(publicPath))
 io.on('connection' , (socket) => {
   console.log('New user connected')
 
-  socket.emit('newMessage' , {
-    from: "jen",
-    text: "Hey how are you",
-    createAt: 12345
-  });
 
   socket.on('createMessage' , (newMessage) => {
     console.log('createMessage' , newMessage);
+    io.emit('newMessage' , {
+      from: newMessage.from,
+      text: newMessage.text,
+      createdAt : new Date().getTime()
+    })
   });
 
   socket.on('disconnect' , () => {
--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,7 @@ var socket = io();
             socket.on('connect' , function () {
                 console.log('connected to server');
 
-            socket.emit('createMessage' , {
-                from : 'bsj',
-                text: 'This is awesome'
-            })
+        
             });
             socket.on('disconnect' , function () {
                 console.log('disconnected to server');

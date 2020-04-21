ef711d8a878fbc4e1f335ddac31e6375f89051a8
Jenith <jenithds@gmail.com>
2020-04-21 12:10:56 +0530

    add newMessage and CreteMessage events

--- Server/server.js
+++ Server/server.js
@@ -15,9 +15,20 @@ app.use(express.static(publicPath))
 io.on('connection' , (socket) => {
   console.log('New user connected')
 
+  socket.emit('newMessage' , {
+    from: "jen",
+    text: "Hey how are you",
+    createAt: 12345
+  });
+
+  socket.on('createMessage' , (newMessage) => {
+    console.log('createMessage' , newMessage);
+  });
+
   socket.on('disconnect' , () => {
     console.log('user was disconnected');
   });
+  
 });
 
 
---
+++ public/js/index.js
@@ -0,0 +1,16 @@
+var socket = io();
+
+            socket.on('connect' , function () {
+                console.log('connected to server');
+
+            socket.emit('createMessage' , {
+                from : 'bsj',
+                text: 'This is awesome'
+            })
+            });
+            socket.on('disconnect' , function () {
+                console.log('disconnected to server');
+            });
+            socket.on('newMessage' , function (message) {
+                console.log('New Message' , message);
+            });

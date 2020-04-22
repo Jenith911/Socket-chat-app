17aea9a1be2decc4c04d2acfa090d145ed092984
Jenith <jenithds@gmail.com>
2020-04-22 21:27:39 +0530

    Add Join page

--- Server/utils/message.js
+++ Server/utils/message.js
@@ -1,15 +1,16 @@
+var moment = require('moment'); 
 var generateMeassage = (from , text) => {
     return {
         from,
         text,
-        createAt: new Date().getTime()
+        createAt: moment().valueOf()
     };
 };
 var generateLocationMessage = (from, latitude, longitude) => {
     return {
       from,
       url: `https://www.google.com/maps?q=${latitude},${longitude}`,
-      createdAt: new Date().getTime()
+      createdAt: moment().valueOf()
     };
   };
 module.exports = {generateMeassage, generateLocationMessage}

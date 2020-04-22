dfeb8ed636634b00c801e8cd9ed3b66bef894e7d
Jenith <jenithds@gmail.com>
2020-04-22 18:00:44 +0530

    Add css for chat page

--- Server/utils/message.js
+++ Server/utils/message.js
@@ -3,6 +3,13 @@ var generateMeassage = (from , text) => {
         from,
         text,
         createAt: new Date().getTime()
-    }
-}
-module.exports = {generateMeassage}
+    };
+};
+var generateLocationMessage = (from, latitude, longitude) => {
+    return {
+      from,
+      url: `https://www.google.com/maps?q=${latitude},${longitude}`,
+      createdAt: new Date().getTime()
+    };
+  };
+module.exports = {generateMeassage, generateLocationMessage}
